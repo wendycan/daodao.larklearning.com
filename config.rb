0358210e86ea2901cbf7a8d1028f5f7f0e479fee
Wendy Wang <wilxnwang@gmail.com>
2015-03-10 19:19:36 +0800

    Add middleman blog

--- config.rb
+++ config.rb
@@ -52,6 +52,31 @@ page "/index.html", :layout => false
 #     "Helping"
 #   end
 # end
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+  blog.name = 'cn'
+  blog.prefix = 'cn'
+  # blog.tag_template = "tag.html"
+  # blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
+end
 
 set :css_dir, 'stylesheets'
 
